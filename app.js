b8e31e946c1b933a9654784e0aeee610f1811b36
Adrianoespanho <112988600+Adrianoespanho@users.noreply.github.com>
2022-10-18 21:07:02 -0300

    Programa de calculo de media

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("Olá, estou funcionando")
+// console.log(10+15)
+// console.log(60/5+17)
+// console.log(47-23)
+let nome= "Adriano"
+let nota1 = 9.3
+let nota2 = 7.2
+let nota3 = 4.5
+let nota4 = 2.33
+let media = (nota1 + nota2 +nota3 + nota4) / 4 
+let mediafixada = media.toFixed(1)
+
+console. log ("Olá" + nome)
+console. log("sua media foi:" + mediafixada)
